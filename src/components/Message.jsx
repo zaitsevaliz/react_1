2d171505c4a203f9db08c6a5c4e3fe69304d02bf
zaitsevaliz <101466989+zaitsevaliz@users.noreply.github.com>
2022-08-20 20:25:03 +0300

    dz1

---
+++ src/components/Message.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export const Message = ({ text }) => {
+    return <>
+        <h3>Компонент Message:</h3>
+        <p> Text from parent by props: {text}</p>
+    </>
+
+}
